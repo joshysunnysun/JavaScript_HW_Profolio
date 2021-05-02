97b88ec15afdfd3fd44abac3b15ef60209a87384
joshysunnysun <ji1997819@gmail.com>
2021-05-02 21:13:07 +0800

    first upload

---
+++ MouseMovePractice/main.js
@@ -0,0 +1,27 @@
+let thisH1 = document.getElementsByTagName("h1")[0];
+let thisP = document.getElementsByTagName("p")[0];
+let thisDiv = document.getElementsByTagName("div")[0];
+
+function mouseIn(){
+    thisH1.innerHTML = "你進來了";
+}
+function mouseOut(){
+    thisH1.innerHTML = "你出去了";
+    thisP.innerHTML = "";
+}
+
+function mouseMove(e){
+    thisP.innerHTML = "你在裡面走來走去。位置:"+e.clientX + "," + e.clientY;
+}
+function mouseClick(){
+    thisP.innerHTML = "點屁點";
+}
+function mouseUp(){
+    thisP.innerHTML = "就跟妳說放手";
+}
+
+thisDiv.addEventListener("mouseover",mouseIn);
+thisDiv.addEventListener("mouseout",mouseOut);
+thisDiv.addEventListener("mousemove",mouseMove);
+thisDiv.addEventListener("mousedown",mouseClick);
+thisDiv.addEventListener("mouseup",mouseUp);
